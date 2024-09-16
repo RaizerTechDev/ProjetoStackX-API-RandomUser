9777e059f0525fe66a4d2ed74a29789d7c51ce14
RafaRz76Dev <rafarz76dev@gmail.com>
2024-09-16 19:53:17 -0300

    :recycle: refactor:  file/ .env and server.js and vercel.json

--- server.js
+++ server.js
@@ -1,60 +1,66 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const axios = require('axios');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Configurando o EJS como motor de views
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Configurar o Express para servir arquivos estáticos da pasta 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-require('dotenv').config();
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true, // Usa o novo analisador de URL do MongoDB
-    useUnifiedTopology: true // Usa o novo mecanismo de topologia unificada
-})
-.then(() => console.log('Conectado ao MongoDB Atlas'))
-.catch(err => console.error('Erro ao conectar ao MongoDB Atlas', err));
+// Conectar ao MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => console.log("Conectado ao MongoDB Atlas"))
+  .catch((err) => console.error("Erro ao conectar ao MongoDB Atlas", err));
 
-  const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    dob: String,
-    age: Number,
-    picture: String // Adicionando o campo da imagem
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  dob: String,
+  age: Number,
+  picture: String,
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-app.get('/fetch-users', async (req, res) => {
-    try {
-        const response = await axios.get('https://randomuser.me/api/');
-        const userData = response.data.results[0];
-        const newUser = new User({
-            name: `${userData.name.first} ${userData.name.last}`,
-            email: userData.email,
-            dob: userData.dob.date,
-            age: userData.dob.age,
-            picture: userData.picture.large 
-        });
-        await newUser.save();
-        res.redirect('/');
-    } catch (error) {
-        res.status(500).send('Erro ao buscar usuários da API');
-    }
+// Rota para buscar e salvar usuários
+app.get("/fetch-users", async (req, res) => {
+  try {
+    const response = await axios.get("https://randomuser.me/api/");
+    const userData = response.data.results[0];
+    const newUser = new User({
+      name: `${userData.name.first} ${userData.name.last}`,
+      email: userData.email,
+      dob: userData.dob.date,
+      age: userData.dob.age,
+      picture: userData.picture.large,
+    });
+    await newUser.save();
+    res.redirect("/");
+  } catch (error) {
+    console.error("Erro ao buscar usuários da API:", error);
+    res.status(500).send("Erro ao buscar usuários da API");
+  }
 });
 
 // Rota para exibir os usuários salvos
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
+  try {
     const users = await User.find();
-    res.render('index', { users });
+    res.render("index", { users });
+  } catch (error) {
+    console.error("Erro ao buscar usuários do banco de dados:", error);
+    res.status(500).send("Erro ao buscar usuários do banco de dados");
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
