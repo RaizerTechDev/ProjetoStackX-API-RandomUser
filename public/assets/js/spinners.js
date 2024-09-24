53dfb7512e321636d38d9519472802bad960c263
RafaRz76Dev <rafarz76dev@gmail.com>
2024-09-24 14:11:26 -0300

    :lipstick: feat(css): Add styles.css to public/css :recycle: refactor: - Improve spinner.js for better performance and usability.

--- public/assets/js/spinners.js
+++ public/assets/js/spinners.js
@@ -1,10 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('addUserButton').addEventListener('click', function() {
-        // Exibir o spinner
-        document.getElementById('spinner').style.display = 'inline-block';
-        console.log('Spinner exibido');
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("addUserButton")
+    .addEventListener("click", function () {
+      // Exibir o spinner
+      document.getElementById("spinner").style.display = "inline-block";
 
-        // Desabilitar o botão para evitar múltiplos cliques
-        this.classList.add('disabled');
+      // Desabilitar o botão para evitar múltiplos cliques
+      this.classList.add("disabled");
     });
 });
